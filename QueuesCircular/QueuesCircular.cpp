f9e66779fd4fb8e7bc86075d013676c3ba73bea8
windyaan <windyanurmaretha@gmail.com>
2023-06-22 11:21:06 +0700

    Pilihan menu

--- QueuesCircular/QueuesCircular.cpp
+++ QueuesCircular/QueuesCircular.cpp
@@ -103,5 +103,14 @@ int main() {
 	Queues q;
 	char ch;
 
-
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): " << endl;
+		}
+	}
 }
