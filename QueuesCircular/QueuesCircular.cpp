e22e4179dca5966f770016b5a3db033594d6b3e5
windyaan <windyanurmaretha@gmail.com>
2023-06-22 11:16:12 +0700

    Perulangan

--- QueuesCircular/QueuesCircular.cpp
+++ QueuesCircular/QueuesCircular.cpp
@@ -87,6 +87,14 @@ public:
 				FRONT_position++;
 			}
 
+			FRONT_position = 0;
+
+			// Iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 	}
 };
