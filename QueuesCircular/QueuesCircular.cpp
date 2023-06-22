5d7f6d7568ef585a8c916819276b71e4f9c1aa85
windyaan <windyanurmaretha@gmail.com>
2023-06-22 11:08:54 +0700

    Perulangan jika

--- QueuesCircular/QueuesCircular.cpp
+++ QueuesCircular/QueuesCircular.cpp
@@ -71,5 +71,15 @@ public:
 		}
 
 		cout << "\nElement is in the queue are: \n";
+
+		// Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+
 	}
 };
