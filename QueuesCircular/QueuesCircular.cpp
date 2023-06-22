10427420842ab92939068efd35e743a533dd88a5
windyaan <windyanurmaretha@gmail.com>
2023-06-22 11:13:06 +0700

    Perulangan dari FRONT hingga akhir array

--- QueuesCircular/QueuesCircular.cpp
+++ QueuesCircular/QueuesCircular.cpp
@@ -80,6 +80,13 @@ public:
 			}
 			cout << endl;
 		}
+		else {
+			// Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
 
+		}
 	}
 };
