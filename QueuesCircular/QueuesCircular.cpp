acba7ac468f7e68535b4c2b32440b583f7fd9ecf
windyaan <windyanurmaretha@gmail.com>
2023-06-22 11:29:44 +0700

    Pointer menuju method

--- QueuesCircular/QueuesCircular.cpp
+++ QueuesCircular/QueuesCircular.cpp
@@ -111,6 +111,31 @@ int main() {
 			cout << "3. Display values" << endl;
 			cout << "4. Exit" << endl;
 			cout << "Enter your choice (1-4): " << endl;
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case'2': {
+				q.remove();
+				break;
+			}
+			case'3': {
+				q.display();
+				break;
+			}
+			case'4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option!!" << endl;
+				break;
+			}
+			}
 		}
 	}
 }
