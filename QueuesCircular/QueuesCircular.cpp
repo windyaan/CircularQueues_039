9d650e1702361219324b3be5892e06bec0e88ff9
windyaan <windyanurmaretha@gmail.com>
2023-06-22 11:02:05 +0700

    Display jika antrian kosong

--- QueuesCircular/QueuesCircular.cpp
+++ QueuesCircular/QueuesCircular.cpp
@@ -64,6 +64,11 @@ public:
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
 
+		// Cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
 
 	}
 };
